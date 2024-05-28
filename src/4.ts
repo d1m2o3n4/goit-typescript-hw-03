235e31659df2576ff803868e0b21a3bba30a6547
d1m2o3n4 <d1m2o3n4@gmail.com>
2024-05-29 02:24:37 +0300

    crt

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,37 @@
+class Key {
+  private sinnature: number = Math.random();
+  getSignature(): number {
+    return this.sinnature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    } else {
+      this.door = false;
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +40,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
